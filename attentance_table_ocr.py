422258b588980a065bd2f73b197200596fe64530
Jinhyo Shin <50395556+realblack0@users.noreply.github.com>
2022-09-30 16:56:25 +0900

    dev3

--- attentance_table_ocr.py
+++ attentance_table_ocr.py
@@ -11,6 +11,8 @@ import pandas as pd
 # eps_pixel = 20 
 # padding = 20 
 # thresh_intensity = 20
+# n_roi_rows = 5
+# batch_progress_bar = True
 
 reader = easyocr.Reader(['ko','en']) # this needs to run only once to load the model into memory
 
@@ -89,34 +91,38 @@ def gererate_roi(feat_image, rgb_image, file_format, padding):
         # horizon region of interest
         hor_position = np.where(feat_image.sum(axis=0) > 0)[0]
         # vertical region of interest
-        ver_sum = feat_image.sum(axis=1)
-        effective_ver_sum = ver_sum[np.where(ver_sum > 0)]
-        ver_thresh = effective_ver_sum.mean() / 2
-        ver_position = np.where(feat_image.sum(axis=1) > ver_thresh)[0]
+        coi_ind = np.argmax(feat_image.sum(axis=0))
+        coi = feat_image[:, coi_ind] # column of interest
+        ver_position = np.where(coi > 0)[0] 
         # crop roi
-        feat_roi = feat_image[ver_position[0]-padding:ver_position[-1]+padding, 
+        feat_roi = feat_image[ver_position[0]-padding:, 
                     hor_position[0]-padding:hor_position[-1]+padding]
                     
-        rgb_roi = rgb_image[ver_position[0]-padding:ver_position[-1]+padding, 
+        rgb_roi = rgb_image[ver_position[0]-padding:, 
                         hor_position[0]-padding:hor_position[-1]+padding, :]
     elif file_format == "pdf":
+        # crop head line
+        hor_sum = feat_image.sum(axis=1) > 0
+        hor_sum_bin = hor_sum.astype(int)
+        ver_something = np.where((hor_sum_bin[1:] - hor_sum_bin[:-1]) == 1)[0]
+        feat_image_crop = feat_image[ver_something[1]-padding:, :]
+        rgb_image_crop = rgb_image[ver_something[1]-padding:, :]
         # vertical region of interest
-        vertical_roi = feat_image.sum(axis=1) > 0
-        vertical_roi = vertical_roi.astype(int)
-        ver_position = np.where((vertical_roi[1:] - vertical_roi[:-1]) == 1)[0]
+        coi_ind = np.argmax(feat_image_crop.sum(axis=0))
+        coi = feat_image_crop[:, coi_ind] # column of interest
+        ver_position = np.where(coi > 0)[0] 
         # horizon region of interest
-        feat_image_crop = feat_image[ver_position[1]-padding:, :]
         horizontal_roi = feat_image_crop.sum(axis=0) > 0
         horizontal_roi = horizontal_roi.astype(int)
         hor_position = np.where((horizontal_roi[1:] - horizontal_roi[:-1]) == 1)[0]
         # crop roi
         padding = 10
-        feat_roi = feat_image[ver_position[1]-padding:, 
-                    hor_position[0]-padding:]
+        feat_roi = feat_image_crop[ver_position[0]-padding:, 
+                                hor_position[0]-padding:]
                     
-        rgb_roi = rgb_image[ver_position[1]-padding:, 
-                        hor_position[0]-padding:, 
-                        :]
+        rgb_roi = rgb_image_crop[ver_position[0]-padding:, 
+                                hor_position[0]-padding:, 
+                                :]
     else :
         raise ValueError("'{}' format is not supported. Only ['png', 'pdf'] are supported.".format(file_format))
     return feat_roi, rgb_roi
@@ -127,12 +133,16 @@ def post_process(row):
     if len(row) == 10:
         row = row[1:]
     mapper = {
-        "0":["o", "O", "U"],
-        "1":["i", "l", "I"], 
+        "0":["o", "O", "U", "D", "C"],
+        "1":["i", "l", "I", "t"], 
         "2":["z", "Z"],
+        "4":["-", "A"],
         "5":["s", "S"],
         "7":["T"],
-        "8":["G", "E", "B"],
+        "8":["G", "E", "B", "e", "@"],
+        "9":["g"],
+        "17":["V"],
+        "h":["+"],
         "m":["r"],
     }
     for i in range(1, len(row)):
@@ -140,25 +150,29 @@ def post_process(row):
             for value in values:
                 row[i] = row[i].replace(value, key)
     return row
+    return row
 
 def ocr(image, eps_pixel):
     result = reader.readtext(image)
     row = []
     rows = []
-    temp = 999
+    prev_ty = -999
     for position, text, _ in result:
-        if abs(temp - position[0][1]) < eps_pixel:
-            row.append(text)
+        if abs(prev_ty - position[0][1]) < eps_pixel:
+            row.append((position, text))
         else:
-            row = post_process(row)
-            rows.append(row)
-            temp = position[0][1]
-            row = [text]
+            sorted_row = sorted(row, key=lambda x:x[0][0][0]) # by left x position
+            row_texts = [_[1] for _ in sorted_row]
+            row_pp = post_process(row_texts)
+            rows.append(row_pp)
+            prev_ty = position[0][1] # previous top y position
+            row = [(position, text)]
     rows.pop(0) # 첫번째 빈 list
-    row = post_process(row)
-    rows.append(row) # 마지막 row
+    sorted_row = sorted(row, key=lambda x:x[0][0][0]) # by left x position
+    row_texts = [_[1] for _ in sorted_row]
+    row_pp = post_process(row_texts)
+    rows.append(row_pp) # 마지막 row
     return rows
-reader = easyocr.Reader(['ko','en']) # this needs to run only once to load the model into memory
 
 
 """find rows"""
@@ -174,12 +188,10 @@ def find_end_of_rows(feat_roi):
 def ocr_by_batched_row(feat_roi, rgb_roi, padding, n_roi_rows, eps_pixel, progress_bar=False):
     end_of_rows = find_end_of_rows(feat_roi)
 
-    # n_roi_rows = 5
     n_steps = len(end_of_rows) // n_roi_rows 
     n_steps = n_steps + 1 if len(end_of_rows) % n_roi_rows else n_steps
 
     total = []
-    temp = []
     a = None
     b = -padding
     if progress_bar is True:
@@ -189,23 +201,22 @@ def ocr_by_batched_row(feat_roi, rgb_roi, padding, n_roi_rows, eps_pixel, progre
     for step in steps:
         a = b
         b = end_of_rows[step*n_roi_rows]
-        crop_image1 = rgb_roi[a+padding:b+padding, : ,:]
+        crop_image1 = rgb_roi[a+padding:b+padding, :, :]
         rows = ocr(crop_image1, eps_pixel)
-        temp.append(rows)
         total = total + rows
         # plt.imshow(crop_image1)
         # plt.show()
     else:
-        crop_image1 = rgb_roi[b+padding:, : ,:]
+        crop_image1 = rgb_roi[b+padding:, :, :]
         rows = ocr(crop_image1, eps_pixel)
         total = total + rows
         # plt.imshow(crop_image1)
         # plt.show()
+    total = [row for row in total if len(row)>4]
     return total
 
 def total_to_df(total):
     df = pd.DataFrame(total)
-    df = df.dropna().reset_index(drop=True)
     df = df.rename(columns={0:"이름", 1:"계", 2:"월", 3:"화", 4:"수", 5:"목", 6:"금", 7:"토", 8:"일"})
     return df
 
@@ -231,8 +242,10 @@ def extract_table_from_image(
         df (pandas.DataFrame) : the extracted table 
     """
     feat_image = generate_feat_image(rgb_image, thresh_intensity)
-    feat_roi, rgb_roi = gererate_roi(feat_image, rgb_image, file_format, padding)
+    try:
+        feat_roi, rgb_roi = gererate_roi(feat_image, rgb_image, file_format, padding)
+    except IndexError:
+        return pd.DataFrame([])
     total = ocr_by_batched_row(feat_roi, rgb_roi, padding=padding, n_roi_rows=n_roi_rows, eps_pixel=eps_pixel, progress_bar=batch_progress_bar)
     df = total_to_df(total) # list -> df
     return df
-
